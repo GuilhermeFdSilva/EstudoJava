6fe23bb6051482dad6abf5fb368ac415d6741667
GuilhermeFdSilva <francaguilherme27@gmail.com>
2022-06-21 21:52:34 -0300

    Continuação Estudos Java

---
+++ curso-java/exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,9 @@
+package classe;
+
+public class PrimeiroTrauma {
+	int a = 3;
+	public static void main(String[] args) {
+		PrimeiroTrauma p = new PrimeiroTrauma();
+		System.out.println(p.a);
+	}
+}
