fb056d36e3183a14172df2c0e4bf0b5f0bef2308
GuilhermeFdSilva <francaguilherme27@gmail.com>
2022-07-20 20:25:19 -0300

    Estudos Java

--- curso-java/exercicios/src/streams/Aluno.java
+++ curso-java/exercicios/src/streams/Aluno.java
@@ -1,10 +1,35 @@
 package streams;
 
+import java.util.Objects;
+
 public class Aluno {
 	final String nome;
 	final double nota;
+	final boolean bomComportamento;
 	public Aluno(String nome, double nota) {
+		this(nome, nota, true);
+	}
+	public Aluno(String nome, double nota, boolean bomComportamento) {
 		this.nome = nome;
 		this.nota = nota;
+		this.bomComportamento = bomComportamento;
 	}
+	public String toString() {
+		return nome + " tem nota " + nota;
+	}
+	public int hashCode() {
+		return Objects.hash(bomComportamento, nome, nota);
+	}
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return bomComportamento == other.bomComportamento && Objects.equals(nome, other.nome)
+				&& Double.doubleToLongBits(nota) == Double.doubleToLongBits(other.nota);
+	}
+	
 }
---
+++ curso-java/exercicios/src/streams/Outros.java
@@ -0,0 +1,31 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Outros {
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno("Ana", 7.1);
+		Aluno a2 = new Aluno("Luna", 6.1);
+		Aluno a3 = new Aluno("Gui", 8.1);
+		Aluno a4 = new Aluno("Gabi", 10);
+		Aluno a5 = new Aluno("Ana", 7.1);
+		Aluno a6 = new Aluno("Pedro", 6.1);
+		Aluno a7 = new Aluno("Gui", 8.1);
+		Aluno a8 = new Aluno("Maria", 10);
+		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8);
+		System.out.println("Distinct...");
+		alunos.stream().distinct().forEach(System.out::println);
+		System.out.println("\nSkip/Limit...");
+		alunos.stream()
+		.distinct()
+		.skip(2)
+		.limit(2)
+		.forEach(System.out::println);
+		System.out.println("\nTakeWhile...");
+		alunos.stream()
+		.distinct()
+		.takeWhile(a -> a.nota >= 7)
+		.forEach(System.out::println);
+	}
+}
