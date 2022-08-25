4015ec7cf331022dc34f520b1df7e290369c32b9
GuilhermeFdSilva <francaguilherme27@gmail.com>
2022-08-25 12:12:45 -0300

    Estudos Java

--- curso-java/exercicios/src/banco-relacional/ConsultarEstado.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM estados
-
-SELECT nome AS 'Nome do Estado', sigla FROM estados
-WHERE regiao = 'Sul'
-
-SELECT nome, populacao, regiao FROM estados
-WHERE populacao >= 10
-ORDER BY populacao DESC
