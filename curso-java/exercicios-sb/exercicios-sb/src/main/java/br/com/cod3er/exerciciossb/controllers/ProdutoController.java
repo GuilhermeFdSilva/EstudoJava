a61968f3ffdbee73f3e30ffa20d66a572f610173
Guilherme <francaguilherme27@gmail.com>
2023-05-04 12:44:21 -0300

    Estudos Java

--- curso-java/exercicios-sb/exercicios-sb/src/main/java/br/com/cod3er/exerciciossb/controllers/ProdutoController.java
+++ curso-java/exercicios-sb/exercicios-sb/src/main/java/br/com/cod3er/exerciciossb/controllers/ProdutoController.java
@@ -2,11 +2,11 @@ package br.com.cod3er.exerciciossb.controllers;
 
 import br.com.cod3er.exerciciossb.models.entities.Produto;
 import br.com.cod3er.exerciciossb.models.repositories.ProdutoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -14,8 +14,20 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
     @PostMapping
-    public Produto novoProduto(@RequestParam String nome){
-        Produto produto = new Produto(nome);
+    public Produto novoProduto (@Valid Produto produto){
+        produtoRepository.save(produto);
+        return produto;
+    }
+    @GetMapping
+    public Iterable<Produto> obterProdutos(){
+        return produtoRepository.findAll();
+    }
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id){
+        return produtoRepository.findById(id);
+    }
+    @PutMapping
+    public Produto alterarProduto(@Valid Produto produto){
         produtoRepository.save(produto);
         return produto;
     }
--- curso-java/exercicios-sb/exercicios-sb/src/main/java/br/com/cod3er/exerciciossb/models/entities/Produto.java
+++ curso-java/exercicios-sb/exercicios-sb/src/main/java/br/com/cod3er/exerciciossb/models/entities/Produto.java
@@ -4,19 +4,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank
     private  String nome;
+    @Min(value = 0)
+    private double preco;
+    @Min(value = 0)
+    @Max(value = 1)
+    private double desconto;
     public Produto(){
-    }
-    public Produto(String nome) {
-        this.nome = nome;
-    }
 
+    }
+    public Produto(String nome, double preco, double desconto){
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
     public int getId() {
         return id;
     }
@@ -28,5 +39,17 @@ public class Produto {
     }
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public double getDesconto() {
+        return desconto;
+    }
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
     }
 }
